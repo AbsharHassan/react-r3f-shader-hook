7185563505242e73f70a161d1846b37900dc0746
AbsharHassan <abshar.hassan7@gmail.com>
2023-12-12 15:31:44 +0500

    installed drei

--- src/stories/Scene.tsx
+++ src/stories/Scene.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
+import { Box, OrbitControls } from '@react-three/drei'
 
 type Props = {}
 
 const Scene = (props: Props) => {
   return (
-    <Canvas style={{ width: '100vw', height: '100vh' }}>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial />
+    <Canvas
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [-2, 2, 6], fov: 50, near: 1, far: 20 }}
+      style={{ width: '100%', height: '100vh' }}
+    >
+      <OrbitControls />
+      <color
+        attach="background"
+        args={['#202020']}
+      />
+      <fog
+        attach="fog"
+        args={['#202020', 5, 20]}
+      />
+      <ambientLight intensity={0.015} />
+
+      <mesh receiveShadow>
+        <planeGeometry args={[50, 10]} />
+        <meshBasicMaterial
+          color="#f00"
+          transparent
+          opacity={1}
+        />
       </mesh>
+      <Box></Box>
     </Canvas>
   )
 }
