c04c760cc9eba6daef880b936acf119a427cd580
AbsharHassan <abshar.hassan7@gmail.com>
2023-12-12 17:45:46 +0500

    created example Scene with box

--- src/stories/Experience/Scene.tsx
+++ src/stories/Experience/Scene.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
-import { useDepthBuffer } from '@react-three/drei'
+import { Box, OrbitControls, useDepthBuffer } from '@react-three/drei'
+import MovingSpot from './MovingSpot'
 
-type Props = {}
-
-const Scene = (props: Props) => {
+const Scene = () => {
   const depthBuffer = useDepthBuffer({ frames: 1 })
 
-  return <div>Scene</div>
+  return (
+    <>
+      <OrbitControls />
+
+      <MovingSpot
+        depthBuffer={depthBuffer}
+        color="#0c8cbf"
+        position={[3, 3, 2]}
+      />
+
+      <Box
+        position={[0, 0, 0.2]}
+        castShadow
+      >
+        <meshPhysicalMaterial />
+      </Box>
+
+      <mesh
+        receiveShadow
+        position={[0, -0.5, 0]}
+        rotation-x={-Math.PI / 2}
+      >
+        <planeGeometry args={[50, 50]} />
+        <meshPhongMaterial />
+      </mesh>
+    </>
+  )
 }
 
 export default Scene
