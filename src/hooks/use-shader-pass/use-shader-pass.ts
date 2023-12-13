422d620cd2909310e2444973995b2307a6a9843b
AbsharHassan <abshar.hassan7@gmail.com>
2023-12-13 13:28:46 +0500

    typing hook params

--- src/hooks/use-shader-pass/use-shader-pass.ts
+++ src/hooks/use-shader-pass/use-shader-pass.ts
@@ -1,3 +1,22 @@
-const placeholder = 'hello world'
+import { useEffect, useMemo, useState } from 'react'
+import {
+  Scene,
+  OrthographicCamera,
+  BufferGeometry,
+  BufferAttribute,
+  Vector2,
+  WebGLRenderTarget,
+  RGBAFormat,
+  RawShaderMaterial,
+  Mesh,
+  IUniform,
+} from 'three'
+import { useFrame, useThree } from '@react-three/fiber'
 
-export default placeholder
+const useShaderPass = (
+  vertexShader: string,
+  fragmentShader: string,
+  uniforms: { [uniform: string]: IUniform }
+): void => {
+  uniforms
+}
