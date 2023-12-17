19a1c143d6dfcfd0d3decc4a921004dcb766676b
AbsharHassan <abshar.hassan7@gmail.com>
2023-12-18 01:58:53 +0500

    clean up

--- src/stories/Experience/Experience.tsx
+++ src/stories/Experience/Experience.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Scene from './Scene'
