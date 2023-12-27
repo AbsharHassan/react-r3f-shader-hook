e60b63520866d819e8f114e5d2054a6f61bd2d56
AbsharHassan <abshar.hassan7@gmail.com>
2023-12-27 09:37:30 +0500

    transferring test logic to component

--- src/__tests__/helpers/DummyComponentForHookTest.tsx
+++ src/__tests__/helpers/DummyComponentForHookTest.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { RequiredShaderMaterialParameters } from '../../hooks/use-shader-pass/use-shader-pass.types'
+import { useShaderPass } from '../../hooks'
 
 type Props = {}
 
 const DummyComponentForHookTest = (props: Props) => {
-  return <div>DummyComponentForHookTest</div>
+  const vertexShader = `
+        precision highp float;
+        attribute vec2 position;
+        void main() {
+        gl_Position = vec4(position, 1.0, 1.0);
+        }`
+
+  const fragmentShader = `
+        precision highp float;
+        uniform sampler2D uScene;
+        uniform vec2 uResolution;
+        void main() {
+        vec2 uv = gl_FragCoord.xy / uResolution.xy;
+        vec4 color = texture2D(uScene, uv).rgba;
+        gl_FragColor = color;
+        }
+    `
+
+  const initialProps: RequiredShaderMaterialParameters = {
+    vertexShader,
+    fragmentShader,
+  }
+
+  return <mesh />
 }
 
 export default DummyComponentForHookTest
--- src/__tests__/use-shader-pass.test.tsx
+++ src/__tests__/use-shader-pass.test.tsx
@@ -1,36 +1,10 @@
 import React from 'react'
-import { renderHook } from '@testing-library/react'
 import ReactThreeTestRenderer from '@react-three/test-renderer'
-import { useShaderPass } from '../hooks'
-import DummyComponentForHookTest from './helpers/DummyComponentForHookTest'
-import { RequiredShaderMaterialParameters } from '../hooks/use-shader-pass/use-shader-pass.types'
 import { Canvas } from '@react-three/fiber'
+import DummyComponentForHookTest from './helpers/DummyComponentForHookTest'
 
 describe('Implementing', () => {
   it.skip('should successfully call the hook in component', async () => {
-    const vertexShader = `
-        precision highp float;
-        attribute vec2 position;
-        void main() {
-        gl_Position = vec4(position, 1.0, 1.0);
-        }`
-
-    const fragmentShader = `
-        precision highp float;
-        uniform sampler2D uScene;
-        uniform vec2 uResolution;
-        void main() {
-        vec2 uv = gl_FragCoord.xy / uResolution.xy;
-        vec4 color = texture2D(uScene, uv).rgba;
-        gl_FragColor = color;
-        }
-    `
-
-    const initialProps: RequiredShaderMaterialParameters = {
-      vertexShader,
-      fragmentShader,
-    }
-
     const renderer = await ReactThreeTestRenderer.create(
       <Canvas>
         <DummyComponentForHookTest />
