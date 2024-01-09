8eebed018fa707da7a43dc9ad7866741770ed702
AbsharHassan <abshar.hassan7@gmail.com>
2024-01-10 00:02:39 +0500

    refactor to DRY + import to fix Cypress TS conflict

--- src/__tests__/use-shader-pass.test.tsx
+++ src/__tests__/use-shader-pass.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { expect } from '@jest/globals'
 import { create } from '@react-three/test-renderer'
 import { useShaderPass } from '../hooks'
 import { RawShaderMaterial } from 'three'
@@ -34,131 +35,59 @@ const hasKey = <K extends string>(
 }
 
 describe('Implementing', () => {
-  test("hook's return type should be RawShaderMaterial", async () => {
-    let material: any
+  let material: any
 
-    const Component = () => {
-      material = useShaderPass({ vertexShader, fragmentShader })
+  const TestComponent = () => {
+    material = useShaderPass({ vertexShader, fragmentShader })
+    return (
+      <mesh>
+        <boxGeometry args={[2, 2]} />
+        <meshBasicMaterial />
+      </mesh>
+    )
+  }
 
-      return (
-        <mesh>
-          <boxGeometry args={[2, 2]} />
-          <meshBasicMaterial />
-        </mesh>
-      )
-    }
+  const setupTest = async () => {
+    await create(<TestComponent />)
+  }
 
-    await create(<Component />)
+  beforeEach(async () => {
+    await setupTest()
+  })
 
+  test("hook's return type should be RawShaderMaterial", () => {
     expect(material instanceof RawShaderMaterial).toBeTruthy()
   })
 
-  test('material should have the provided VERTEX shader', async () => {
-    let material: any
-
-    const Component = () => {
-      material = useShaderPass({ vertexShader, fragmentShader })
-
-      return (
-        <mesh>
-          <boxGeometry args={[2, 2]} />
-          <meshBasicMaterial />
-        </mesh>
-      )
-    }
-
-    await create(<Component />)
-
+  test('material should have the provided VERTEX shader', () => {
     expect(material.vertexShader).toBe(vertexShader)
   })
 
-  test('material should have the provided FRAGMENT shader', async () => {
-    let material: any
-
-    const Component = () => {
-      material = useShaderPass({ vertexShader, fragmentShader })
-
-      return (
-        <mesh>
-          <boxGeometry args={[2, 2]} />
-          <meshBasicMaterial />
-        </mesh>
-      )
-    }
-
-    await create(<Component />)
-
+  test('material should have the provided FRAGMENT shader', () => {
     expect(material.fragmentShader).toBe(fragmentShader)
   })
 
-  test('material should have the necessary uScene uniform', async () => {
-    let material: any
-
-    const Component = () => {
-      material = useShaderPass({ vertexShader, fragmentShader })
-
-      return (
-        <mesh>
-          <boxGeometry args={[2, 2]} />
-          <meshBasicMaterial />
-        </mesh>
-      )
-    }
-
-    await create(<Component />)
-
+  test('material should have the necessary uScene uniform', () => {
     expect(hasKey(material.uniforms, 'uScene')).toBeTruthy()
   })
 
-  test('material should have the necessary uResolution uniform', async () => {
-    let material: any
-
-    const Component = () => {
-      material = useShaderPass({ vertexShader, fragmentShader })
-
-      return (
-        <mesh>
-          <boxGeometry args={[2, 2]} />
-          <meshBasicMaterial />
-        </mesh>
-      )
-    }
-
-    await create(<Component />)
-
+  test('material should have the necessary uResolution uniform', () => {
     expect(hasKey(material.uniforms, 'uResolution')).toBeTruthy()
   })
 
-  test('material should ONLY have the above uniforms IF none are provided', async () => {
-    let material: any
-
-    const Component = () => {
-      material = useShaderPass({ vertexShader, fragmentShader })
-
-      return (
-        <mesh>
-          <boxGeometry args={[2, 2]} />
-          <meshBasicMaterial />
-        </mesh>
-      )
-    }
-
-    await create(<Component />)
-
+  test('material should ONLY have the above uniforms IF none are provided', () => {
     expect(Object.keys(material.uniforms).length).toBe(2)
   })
 
   test('uniforms should have the provided uniform', async () => {
     let material: any
     const uTest = { value: 1 }
-
     const Component = () => {
       material = useShaderPass({
         vertexShader,
         fragmentShader,
         uniforms: { uTest },
       })
-
       return (
         <mesh>
           <boxGeometry args={[2, 2]} />
@@ -166,13 +95,7 @@ describe('Implementing', () => {
         </mesh>
       )
     }
-
     await create(<Component />)
-
     expect(hasKey(material.uniforms, 'uTest')).toBeTruthy()
   })
-
-  test.todo(
-    'updating the provided uniform should update it in the material as well'
-  )
 })
