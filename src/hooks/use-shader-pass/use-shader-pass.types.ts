4c4d71f9faad4312fd855c22bc2b51da5457660d
AbsharHassan <abshar.hassan7@gmail.com>
2024-01-18 13:09:37 +0500

    feat(FXAA): add parameter to toggle anti-aliasing

--- src/hooks/use-shader-pass/use-shader-pass.types.ts
+++ src/hooks/use-shader-pass/use-shader-pass.types.ts
@@ -3,4 +3,5 @@ import { ShaderMaterialParameters } from 'three'
 export interface UseShaderPassParameters extends ShaderMaterialParameters {
   vertexShader: string
   fragmentShader: string
+  antialias?: boolean
 }
